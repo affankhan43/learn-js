7c50f518f44d1b9b0829da0ede444de527d2e263
unknown <affankhan627@gmail.com>
2020-12-22 02:28:40 +0500

    node

--- class-npm/index.js
+++ class-npm/index.js
@@ -1,18 +1,30 @@
 var http = require('http')
+var url = require('url');
 
 // console.log(http.METHODS)
 // console.log(http.STATUS_CODES)
 
 http.createServer(function(req,res){
+	var q = url.parse(req.url, true).query;
+	console.log(q)
+	console.log(req.body)
+	req.on('data', chunk => {
+        console.log(chunk.toString()); // convert Buffer to string
+    });
+	console.log('URL:-->'+url.parse(req.url, true).pathname)
 	if(req.url == '/'){
 		console.log(req.url)
 		console.log(req.connection.remoteAddress)
 		res.write('<h1>Nodejs Project</h1>' + req.url)
 		res.end()
 	}else if(req.url == '/post'){
-		res.write('Welcome To Post')
+		req.on('data',(dd)=>{
+			console.log(dd)
+		})
+		res.write('asd')
 		res.end()
-	}else{
+	}
+	else{
 		var data = {'text':'asdasds'}
 		var d = 1;
 
